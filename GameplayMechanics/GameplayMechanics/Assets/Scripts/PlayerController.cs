d975e6c7d54b01949d094e285e17eaa0fc26428b
Japnaam Jaswal <jaswalj1@roverkids.org>
2026-05-27 14:07:52 -0400

    Lesson 4.3 Finished

--- GameplayMechanics/GameplayMechanics/Assets/Scripts/PlayerController.cs
+++ GameplayMechanics/GameplayMechanics/Assets/Scripts/PlayerController.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
@@ -5,7 +6,10 @@ public class PlayerController : MonoBehaviour
     private Rigidbody playerRb;
     public float speed = 5.0f;
     private GameObject focalPoint;
-        
+    public bool hasPowerup;
+    private float powerupStrength = 15.0f;
+    public GameObject powerupIndicator;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -17,6 +21,38 @@ public class PlayerController : MonoBehaviour
     void Update()
     {
         float forwardInput = Input.GetAxis("Vertical");
+
         playerRb.AddForce(focalPoint.transform.forward * speed * forwardInput);
+
+        powerupIndicator.transform.position = transform.position + new Vector3(0, -0.5f, 0);
     }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        hasPowerup = true;
+        powerupIndicator.gameObject.SetActive(true);
+        Destroy(other.gameObject);
+        StartCoroutine(PowerupCountdownRoutine());
+    }
+
+    IEnumerator PowerupCountdownRoutine()
+    {
+        yield return new WaitForSeconds(7);
+            hasPowerup = false;
+        powerupIndicator.gameObject.SetActive(false);
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Enemy") && hasPowerup)
+        {
+            Rigidbody enemyRigidbody = collision.gameObject.GetComponent<Rigidbody>();
+            Vector3 awayFromPlayer = collision.gameObject.transform.position - transform.position;
+
+            Debug.Log("Collided With: " + collision.gameObject.name + " with powerup set to " + hasPowerup);
+            enemyRigidbody.AddForce(awayFromPlayer * powerupStrength, ForceMode.Impulse);
+        }
+    }
+
+
 }
--- GameplayMechanics/GameplayMechanics/Assets/Scripts/SpawnManager.cs
+++ GameplayMechanics/GameplayMechanics/Assets/Scripts/SpawnManager.cs
@@ -6,7 +6,7 @@ public class SpawnManager : MonoBehaviour
     private float spawnRange = 9.0f;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
-    {
+    {   
         Instantiate(enemyPrefab, GenerateSpawnPosition(), enemyPrefab.transform.rotation);
     }
 
